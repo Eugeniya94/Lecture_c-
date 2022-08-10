afd8fb48407cd53b8e5cb507525bfe0e2ca6a7f8
Evgeniya <sluchevskaya0309@yandex.ru>
2022-08-10 11:48:18 +0300

    добавила изменения в array и создала файл met.FIND

--- Лекция С# 2/Array/Program.cs
+++ Лекция С# 2/Array/Program.cs
@@ -1,6 +1,6 @@
 ﻿
 
-int Max (int arg1, int arg2, int arg3)
+int Max (int arg1, int arg2, int arg3)   // это функция
 {
     int result = arg1;
     if (arg2 > result) result = arg2;
@@ -8,18 +8,23 @@ int Max (int arg1, int arg2, int arg3)
     return result;
 
     }
+// index        0  1  2   3   4   5  6  7   8
+int [] array = {1, 3, 5, 42, 50, 66, 7, 4, 95};
+
+ //array[0] = 12; // присваивает индексу 0 значение 12. после текущего кода будет присвоено это значение
+
+//Console.WriteLine(array[0]); // показывает значение по индексу.
 
 
-// int max1 = Max(a1, b1, c1);
-// int max2 = Max(a2, b2, c2);
-// int max3 = Max(a3, b3, c3);
-// int max = Max(max1, max2, max3);
+int result /* или (max)*/ = Max (
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+    );
+    
+    
+Console.WriteLine(result);
 
-int max = Max (Max(a1, b1, c1),
-               Max(a2, b2, c2),
-               Max(a3, b3, c3));
-
-Console.WriteLine(max);
 
 
 
