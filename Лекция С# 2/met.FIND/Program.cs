be599dd6238c6cfc0c9691084a8e616e89895482
Evgeniya <sluchevskaya0309@yandex.ru>
2022-08-10 12:03:00 +0300

    Сохранила find и создала random

--- Лекция С# 2/met.FIND/Program.cs
+++ Лекция С# 2/met.FIND/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int [] array = {1, 3, 5, 42, 50, 66, 5, 7, 4, 95};
+
+int n = array.Length;
+int find = 5;
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // оператор нужен, когда в массиве несколько одинаковых искомых элементов.
+                // он прерывает цикл при нахождении первого нужного элемента.
+    }
+    index++;
+}
