aaec8feb5d25d6bbb57248cfbc50c0a8c4f86bb5
Evgeniya <sluchevskaya0309@yandex.ru>
2022-08-17 15:41:30 +0300

    законспектировала лекцию 3

---
+++ циклы/текст/Program.cs
@@ -0,0 +1,28 @@
+﻿// Дан текст. В тексте все пробелы нужно заменить черточками, 
+// маленькие буквы "к" заменить большими "К", а большие "С" заменить маленькими "с".
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = “qwerty”
+//             012345
+// s[3] // r
+
+string Replase(string text, char oldValue, char newValue) // string text- входной текст; char-конкретный символ
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replase(text, " ", "|");
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replase(text, "к", "К");
+Console.WriteLine(newText);
